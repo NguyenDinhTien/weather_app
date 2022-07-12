adc75d6197f8986a6063d1811bc3d2b90515076b
NguyenDinhTien <tomtc95@gmail.com>
2022-07-13 00:00:39 +0700

    add feature 3

--- lib/app.dart
+++ lib/app.dart
@@ -16,6 +16,7 @@ class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print('add feature 2');
+    print('add feature 3');
     return RepositoryProvider.value(
       value: _weatherRepository,
       child: BlocProvider(
