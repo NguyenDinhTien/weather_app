1d28b333d41601db5b3934e1c58752fd01296936
NguyenDinhTien <tomtc95@gmail.com>
2022-07-12 23:54:27 +0700

    remove all feature

--- lib/app.dart
+++ lib/app.dart
@@ -32,8 +32,6 @@ class WeatherAppView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('this is a new line');
-    print('this is a new line two');
     final textTheme = Theme.of(context).textTheme;
     return MaterialApp(
       theme: ThemeData(
